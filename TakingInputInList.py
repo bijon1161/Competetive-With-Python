892043dc4c79186098c2222ff56910a4b8511694
bijon <bijonndc@gmail.com>
2021-05-27 21:24:19 +0600

    Taking Input on List

---
+++ TakingInputInList.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 27 19:29:50 2021
+
+@author: Admin
+"""
+
+in1 = input("Enter a list element separated by space: ")
+list = in1.split()
+print(list)
