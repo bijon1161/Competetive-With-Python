80444b54f3234b2ece4ed56a001b4406c38099b0
bijon <bijonndc@gmail.com>
2021-06-01 13:38:36 +0600

    Insert function test

---
+++ ListInsert.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  1 13:35:18 2021
+
+@author: Admin
+"""
+def lisInsert():
+    lis = [1,2,3,4]
+    lis.insert(1,5) #(index,element)
+    print(lis)
